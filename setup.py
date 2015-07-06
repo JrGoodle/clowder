d89dbd1799a14a0d85620f010c424598d7e89544
joe DeCapo <joe@polka.cat>
2015-07-06 03:39:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,55 +1,41 @@
 import os
-import setuptools
+from setuptools import setup
 import sys
 
 # Written according to the docs at
 # https://packaging.python.org/en/latest/distributing.html
 
-project_root = os.path.dirname(__file__)
-peru_sh_path = os.path.join(project_root, 'clowder.sh')
-module_root = os.path.join(project_root, 'clowder')
-version_file = os.path.join(module_root, 'VERSION')
-
-
-def get_version():
-    with open(version_file) as f:
-        return f.read().strip()
-
-
-def get_all_resources_filepaths():
-    resources_paths = ['VERSION']
-    resources_dir = os.path.join(module_root, 'resources')
-    for dirpath, dirnames, filenames in os.walk(resources_dir):
-        relpaths = [os.path.relpath(os.path.join(dirpath, f),
-                                    start=module_root)
-                    for f in filenames]
-        resources_paths.extend(relpaths)
-    return resources_paths
-
-
-def get_install_requires():
-    dependencies = ['gitpython']
-    # Python 3.3 needs extra libs that aren't installed by default.
-    if sys.version_info < (3, 3):
-        raise RuntimeError('The minimum supported Python version is 3.3.')
-    elif (3, 3) <= sys.version_info < (3, 4):
-        # dependencies.extend(['asyncio', 'pathlib'])
-    return dependencies
-
-
-setuptools.setup(
+setup(
     name='clowder',
     description='A tool for managing code',
-    version=get_version(),
+    version='0.1.0',
     url='https://github.com/jrgoodle/clowder',
-    author="joe DeCapo <joe@polka.cat>,
+    author='joe DeCapo',
+    author_email='joe@polka.cat',
     license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     packages=['clowder'],
-    package_data={'clowder': get_all_resources_filepaths()},
     entry_points={
         'console_scripts': [
-            'clowder=clowder.clowder',
+            'clowder=clowder.clowder:main',
         ]
     },
-    install_requires=get_install_requires(),
+    install_requires=['gitpython']
 )
