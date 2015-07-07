ba87ff2dbe67a4f3857f3ed515865840313cdfbc
joe DeCapo <joe@polka.cat>
2015-07-07 00:41:58 -0500

    Fix syntax for calling subparser commands

--- clowder/main.py
+++ clowder/main.py
@@ -33,47 +33,44 @@ class Clowder(object):
         subparsers = parser.add_subparsers(dest='subparser_name', help='sub-command help')
 
         parser_breed = subparsers.add_parser('breed', help='breed help', description='Clone repositories')
-        parser_breed.set_defaults(func=self.breed)
         parser_breed.add_argument('url') # TODO: save parameter and validate url
         parser_breed.add_argument('--groups', '-g', nargs='*', choices=self.manifest.getGroups())
 
         parser_herd = subparsers.add_parser('herd', help='herd help', description='Sync repositories')
-        parser_herd.set_defaults(func=self.herd)
         versions = ['master']
         versions.extend(self.manifest.getVersions())
         parser_herd.add_argument('--version', '-v', choices=versions)
         parser_herd.add_argument('--groups', '-g', nargs='*', choices=self.manifest.getGroups())
 
         parser_play = subparsers.add_parser('play', help='play help', description='Create new topic branch(es)')
-        parser_play.set_defaults(func=self.play)
         parser_play.add_argument('branch')
         parser_play.add_argument('projects', nargs='*', choices=self.projectManager.getProjectNames())
 
         parser_purr = subparsers.add_parser('purr', help='purr help', description='Commit and upload current peru.yaml')
-        parser_purr.set_defaults(func=self.purr)
 
         parser_meow = subparsers.add_parser('meow', help='meow help', description='Print status of current repositories')
-        parser_meow.set_defaults(func=self.meow)
 
         parser_knead = subparsers.add_parser('knead', help='knead help', description='Show diffs for current repositories')
-        parser_knead.set_defaults(func=self.knead)
 
         parser_litter = subparsers.add_parser('litter', help='litter help', description='Discard local changes')
-        parser_litter.set_defaults(func=self.litter)
         parser_litter.add_argument('projects', nargs='*', choices=self.projectManager.getProjectNames())
 
         parser_groom = subparsers.add_parser('groom', help='groom help', description='Prune obsolete remote branches')
-        parser_groom.set_defaults(func=self.groom)
 
         parser_fix = subparsers.add_parser('fix', help='fix help', description='Save a version and tag it')
-        parser_fix.set_defaults(func=self.fix)
         parser_fix.add_argument('version')
 
         parser_nest = subparsers.add_parser('nest', help='nest help')
-        parser_nest.set_defaults(func=self.nest)
 
         argcomplete.autocomplete(parser)
         self.args = parser.parse_args()
+
+        if not hasattr(self, self.args.subparser_name):
+            print('Unrecognized command')
+            parser.print_help()
+            exit(1)
+        # use dispatch pattern to invoke method with same name
+        getattr(self, self.args.subparser_name)()
 
     def breed(self):
         if os.path.exists(self.clowderDirectory):
