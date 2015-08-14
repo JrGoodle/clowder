24a5fd245a05f705681d4f3c8d820b5d685bcc8d
joe DeCapo <joe@polka.cat>
2015-08-14 18:34:32 -0500

    Add clean.sh utility scripts for example projects

---
+++ examples/cats/clean.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+rm -rf .clowder
+rm -rf black-cats
+rm -rf duke
+rm -rf mu
+rm -rf polkabot
+rm -f clowder.yaml
+rm -f peru.yaml
---
+++ examples/llvm-projects/clean.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+rm -rf .clowder
+rm -rf lldb
+rm -rf llvm
+rm -f clowder.yaml
+rm -f peru.yaml
