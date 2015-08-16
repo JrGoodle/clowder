7c214fc5043f773237ff82f8a2fc2613ee02d177
joe DeCapo <joe@polka.cat>
2015-08-16 00:59:33 -0500

    Add more directories to llvm example clean script

--- examples/llvm-projects/clean.sh
+++ examples/llvm-projects/clean.sh
@@ -2,7 +2,16 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 rm -rf .clowder
+rm -rf klee
+rm -rf libclc
 rm -rf lldb
 rm -rf llvm
+rm -rf lnt
+rm -rf openmp
+rm -rf polly
+rm -rf poolalloc
+rm -rf test-suite
+rm -rf vmkit
+rm -rf zorg
 rm -f clowder.yaml
 rm -f peru.yaml
