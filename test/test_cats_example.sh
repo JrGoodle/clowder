6ef180e5d2ff0170f28de4ac7c7694d468c72af0
joe DeCapo <joe@polka.cat>
2015-08-19 00:22:25 -0500

    Add more commands to test scripts

--- test/test_cats_example.sh
+++ test/test_cats_example.sh
@@ -3,4 +3,5 @@
 cd $TRAVIS_BUILD_DIR/examples/cats
 ./breed.sh && clowder herd -a && ./clean.sh
 ./breed.sh && clowder herd && ./clean.sh
-./breed.sh && clowder herd -v v0.1 && ./clean.sh
+./breed.sh && clowder herd -v v0.1 && clowder herd ./clean.sh
+./breed.sh && clowder herd -a -v v0.1 && clowder herd -a ./clean.sh
