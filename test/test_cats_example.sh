2661b8f8e7299795ad4639f71ce4d321c0c03376
joe DeCapo <joe@polka.cat>
2015-08-21 22:22:46 -0500

    Exit test scripts with error code on failure

--- test/test_cats_example.sh
+++ test/test_cats_example.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd $TRAVIS_BUILD_DIR/examples/cats
-./breed.sh && clowder herd -a && ./clean.sh
-./breed.sh && clowder herd && ./clean.sh
-./breed.sh && clowder herd -v v0.1 && clowder herd && ./clean.sh
-./breed.sh && clowder herd -a -v v0.1 && clowder herd -a && ./clean.sh
+./breed.sh && clowder herd -a && ./clean.sh || exit 1
+./breed.sh && clowder herd && ./clean.sh || exit 1
+./breed.sh && clowder herd -v v0.1 && clowder herd && ./clean.sh || exit 1
+./breed.sh && clowder herd -a -v v0.1 && clowder herd -a && ./clean.sh || exit 1
