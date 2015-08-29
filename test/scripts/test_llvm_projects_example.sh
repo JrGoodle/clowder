e0d4819448d5bed3f06042b7e3b0403f22521a71
joe DeCapo <joe@polka.cat>
2015-08-29 01:55:29 -0500

    Modify test script output formatting

--- test/scripts/test_llvm_projects_example.sh
+++ test/scripts/test_llvm_projects_example.sh
@@ -2,37 +2,38 @@
 
 setup_old_repos()
 {
+    echo 'TEST: Setting up older copies of repos'
     local CLANG_TOOLS_EXTRA_DIR="$LLVM_PROJECTS_DIR/llvm/tools/clang/tools/extra"
     rm -rf $CLANG_TOOLS_EXTRA_DIR
     mkdir -p $CLANG_TOOLS_EXTRA_DIR
     pushd $CLANG_TOOLS_EXTRA_DIR
-    git clone https://github.com/JrGoodle/clang-tools-extra.git .
-    git remote remove origin
-    git remote add origin https://github.com/llvm-mirror/clang-tools-extra.git
-    git fetch
-    git branch -u origin/master
+    git clone https://github.com/JrGoodle/clang-tools-extra.git . &>/dev/null
+    git remote remove origin &>/dev/null
+    git remote add origin https://github.com/llvm-mirror/clang-tools-extra.git &>/dev/null
+    git fetch &>/dev/null
+    git branch -u origin/master &>/dev/null
     popd
 
     local CLANG_DIR="$LLVM_PROJECTS_DIR/llvm/tools/clang"
     rm -rf $CLANG_DIR
     mkdir -p $CLANG_DIR
     pushd $CLANG_DIR
-    git clone https://github.com/JrGoodle/clang.git .
-    git remote remove origin
-    git remote add origin https://github.com/llvm-mirror/clang.git
-    git fetch
-    git branch -u origin/master
+    git clone https://github.com/JrGoodle/clang.git . &>/dev/null
+    git remote remove origin &>/dev/null
+    git remote add origin https://github.com/llvm-mirror/clang.git &>/dev/null
+    git fetch &>/dev/null
+    git branch -u origin/master &>/dev/null
     popd
 
     local COMPILER_RT_DIR="$LLVM_PROJECTS_DIR/llvm/projects/compiler-rt"
     rm -rf $COMPILER_RT_DIR
     mkdir -p $COMPILER_RT_DIR
     pushd $COMPILER_RT_DIR
-    git clone https://github.com/JrGoodle/compiler-rt.git .
-    git remote remove origin
-    git remote add origin https://github.com/llvm-mirror/compiler-rt.git
-    git fetch
-    git branch -u origin/master
+    git clone https://github.com/JrGoodle/compiler-rt.git . &>/dev/null
+    git remote remove origin &>/dev/null
+    git remote add origin https://github.com/llvm-mirror/compiler-rt.git &>/dev/null
+    git fetch &>/dev/null
+    git branch -u origin/master &>/dev/null
     popd
 }
 
@@ -40,13 +41,13 @@ test_branch()
 {
     local git_branch
     git_branch=$(git rev-parse --abbrev-ref HEAD)
-    echo "Current branch: $git_branch"
-    echo "Test branch: $1"
-    [[ "$1" = "$git_branch" ]] && echo "On correct branch: $1" || exit 1
+    echo "TEST: Current branch: $git_branch"
+    echo "TEST: Test branch: $1"
+    [[ "$1" = "$git_branch" ]] && echo "TEST: On correct branch: $1" || exit 1
 }
 
 export LLVM_PROJECTS_DIR="$TRAVIS_BUILD_DIR/examples/llvm-projects"
-pushd $LLVM_PROJECTS_DIR
+cd $LLVM_PROJECTS_DIR
 
 # Test breed and herding
 ./breed.sh && clowder herd || exit 1
