6758c5534751a1ed264a211bbadef0568bf6e778
joe DeCapo <joe@polka.cat>
2015-08-30 08:54:08 -0500

    Refactor status print functions. Don't print path for clowder repo.

--- clowder/utility/print_utilities.py
+++ clowder/utility/print_utilities.py
@@ -24,7 +24,7 @@ def print_clowder_repo_status(root_directory):
     if not os.path.isdir(git_path):
         cprint('clowder', 'green')
         return
-    output = format_repo_string(repo_path, 'clowder', 'clowder')
+    output = format_clowder_string(repo_path, 'clowder')
     print(get_cat_face() + ' ' + output)
 
 def print_project_status(root_directory, path, name):
@@ -36,25 +36,37 @@ def print_project_status(root_directory, path, name):
         return
     print(format_repo_string(repo_path, path, name))
 
+def format_clowder_string(repo_path, name):
+    """Return formatted string for clowder repo"""
+    project_output = format_project_string(repo_path, name)
+    current_ref_output = format_ref_string(repo_path)
+    return project_output + ' ' + current_ref_output
+
 def format_repo_string(repo_path, path, name):
     """Return formatted string for project"""
+    project_output = format_project_string(repo_path, name)
+    current_ref_output = format_ref_string(repo_path)
+    path_output = colored(path, 'cyan')
+    return project_output + ' ' + current_ref_output + ' ' + path_output
+
+def format_project_string(repo_path, name):
+    """Return formatted project name"""
     if git_is_dirty(repo_path):
         color = 'red'
         symbol = '*'
     else:
         color = 'green'
         symbol = ''
-    project_output = colored(symbol + name, color)
+    return colored(symbol + name, color)
 
+def format_ref_string(repo_path):
+    """Return formatted repo ref name"""
     if git_is_detached(repo_path):
         current_ref = git_current_sha(repo_path)
-        current_ref_output = colored('(HEAD @ ' + current_ref + ')', 'magenta')
+        return colored('(HEAD @ ' + current_ref + ')', 'magenta')
     else:
         current_branch = git_current_branch(repo_path)
-        current_ref_output = colored('(' + current_branch + ')', 'magenta')
-
-    path_output = colored(path, 'cyan')
-    return project_output + ' ' + current_ref_output + ' ' + path_output
+        return colored('(' + current_branch + ')', 'magenta')
 
 def print_group(name):
     """Print formatted group name"""
