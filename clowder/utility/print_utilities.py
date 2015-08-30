5af054dd16b592e925763cac6635fed35457102c
joe DeCapo <joe@polka.cat>
2015-08-30 04:41:15 -0500

    Add extra space between cat emoji and text

--- clowder/utility/print_utilities.py
+++ clowder/utility/print_utilities.py
@@ -33,7 +33,7 @@ def print_project_status(root_directory, path, name):
 
     path_output = colored(path, 'cyan')
 
-    print(get_cat() + ' ' + project_output)
+    print(get_cat() + '  ' + project_output)
     print(current_ref_output + ' ' + path_output)
 
 def get_cat():
