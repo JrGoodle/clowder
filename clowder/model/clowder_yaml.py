94372c41fd992c537b8cedb27fbf46cf171a923b
joe DeCapo <joe@polka.cat>
2015-08-31 01:44:47 -0500

    Change location of newlines

--- clowder/model/clowder_yaml.py
+++ clowder/model/clowder_yaml.py
@@ -123,8 +123,8 @@ class ClowderYAML(object):
     def litter(self):
         """Discard changes for all projects"""
         print_clowder_repo_status(self.root_directory)
-        print('')
         git_litter(self.clowder_path)
+        print('')
         for group in self.groups:
             print_group(group.name)
             for project in group.projects:
@@ -144,8 +144,8 @@ class ClowderYAML(object):
     def stash(self):
         """Stash changes for all projects with changes"""
         print_clowder_repo_status(self.root_directory)
-        print('')
         git_stash(self.clowder_path)
+        print('')
         for group in self.groups:
             print_group(group.name)
             for project in group.projects:
