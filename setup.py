7c73dc73af05ff0e0d5e6320451a7a3c6137b6f0
joe DeCapo <joe@polka.cat>
2015-09-02 00:22:21 -0500

    Bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info[0] < 3:
 setup(
     name='clowder',
     description='A tool for managing code',
-    version='0.3.0',
+    version='0.3.1',
     url='https://github.com/jrgoodle/clowder',
     author='joe DeCapo',
     author_email='joe@polka.cat',
