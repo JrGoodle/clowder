2952c7c1f39ce362710a82bd163fc0f37d94369c
joe DeCapo <joe@polka.cat>
2015-09-08 03:41:32 -0500

    Add project unit tests

--- test/test_project.py
+++ test/test_project.py
@@ -4,7 +4,7 @@ from clowder.source import Source
 from clowder.project import Project
 
 CURRENT_FILE_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-CATS_EXAMPLE_PATH = os.path.join(CURRENT_FILE_DIR_PATH, '..', 'examples', 'cats')
+CATS_EXAMPLE_PATH = os.path.abspath(os.path.join(CURRENT_FILE_DIR_PATH, '..', 'examples', 'cats'))
 
 class ProjectTest(unittest.TestCase):
     """project test subclass"""
@@ -19,6 +19,23 @@ class ProjectTest(unittest.TestCase):
                    'path': 'black-cats/kit',
                    'ref': 'da5c3d32ec2c00aba4a9f7d822cce2c727f7f5dd'}
         self.project = Project(CATS_EXAMPLE_PATH, project, defaults, sources)
+
+    def test_exists(self):
+        """Test exists() method"""
+        self.assertTrue(self.project.exists())
+
+    def test_full_path(self):
+        """Test full_path() method"""
+        self.assertEqual(self.project.full_path(), self.project_path)
+
+    def test_member_variables(self):
+        """Test the state of all project member variables initialized"""
+        self.assertEqual(self.project.name, 'jrgoodle/kit')
+        self.assertEqual(self.project.path, 'black-cats/kit')
+        self.assertEqual(self.project.ref, 'da5c3d32ec2c00aba4a9f7d822cce2c727f7f5dd')
+        self.assertEqual(self.project.remote_name, 'origin')
+        self.assertEqual(self.project.root_directory, CATS_EXAMPLE_PATH)
+        self.assertEqual(self.project.url, 'https://github.com/jrgoodle/kit.git')
 
     def test_get_yaml(self):
         """Test get_yaml() method"""
