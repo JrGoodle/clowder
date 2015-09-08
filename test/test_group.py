76809f828fcb4fb70ff761e4b3b3be4fa23559d8
joe DeCapo <joe@polka.cat>
2015-09-08 03:54:00 -0500

    Add unit tests to test_group.py

--- test/test_group.py
+++ test/test_group.py
@@ -1,15 +1,39 @@
 """Test group class"""
-import unittest
+import os, unittest
 from clowder.group import Group
+from clowder.source import Source
+
+CURRENT_FILE_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+CATS_EXAMPLE_PATH = os.path.abspath(os.path.join(CURRENT_FILE_DIR_PATH, '..', 'examples', 'cats'))
 
 class GroupTest(unittest.TestCase):
     """group test subclass"""
     def setUp(self):
-        pass
+        defaults_yaml = {'ref': 'refs/heads/master',
+                         'remote': 'origin',
+                         'source': 'github'}
+        sources = [Source({'name': 'github-ssh', 'url': 'ssh://git@github.com'}),
+                   Source({'name': 'github', 'url': 'https://github.com'})]
+        project_yaml = {'name': 'jrgoodle/kit',
+                        'path': 'black-cats/kit',
+                        'ref': 'da5c3d32ec2c00aba4a9f7d822cce2c727f7f5dd'}
+        group_yaml = {'name': 'cats',
+                      'projects': [project_yaml]}
+        self.group = Group(CATS_EXAMPLE_PATH, group_yaml, defaults_yaml, sources)
+
+    def test_exists(self):
+        """Test exists() method"""
+        self.assertTrue(self.group.exists())
 
     def test_get_yaml(self):
         """Test get_yaml() method"""
-        pass
+        group_yaml = {'name': 'cats',
+                      'projects': [{'name': 'jrgoodle/kit',
+                                    'path': 'black-cats/kit',
+                                    'ref': 'da5c3d32ec2c00aba4a9f7d822cce2c727f7f5dd',
+                                    'remote': 'origin',
+                                    'source': 'github'}]}
+        self.assertEqual(self.group.get_yaml(), group_yaml)
 
 if __name__ == '__main__':
     unittest.main()
--- test/test_project.py
+++ test/test_project.py
@@ -10,15 +10,15 @@ class ProjectTest(unittest.TestCase):
     """project test subclass"""
     def setUp(self):
         self.project_path = os.path.join(CATS_EXAMPLE_PATH, 'black-cats', 'kit')
-        defaults = {'ref': 'refs/heads/master',
-                    'remote': 'origin',
-                    'source': 'github'}
+        defaults_yaml = {'ref': 'refs/heads/master',
+                         'remote': 'origin',
+                         'source': 'github'}
         sources = [Source({'name': 'github-ssh', 'url': 'ssh://git@github.com'}),
                    Source({'name': 'github', 'url': 'https://github.com'})]
-        project = {'name': 'jrgoodle/kit',
-                   'path': 'black-cats/kit',
-                   'ref': 'da5c3d32ec2c00aba4a9f7d822cce2c727f7f5dd'}
-        self.project = Project(CATS_EXAMPLE_PATH, project, defaults, sources)
+        project_yaml = {'name': 'jrgoodle/kit',
+                        'path': 'black-cats/kit',
+                        'ref': 'da5c3d32ec2c00aba4a9f7d822cce2c727f7f5dd'}
+        self.project = Project(CATS_EXAMPLE_PATH, project_yaml, defaults_yaml, sources)
 
     def test_exists(self):
         """Test exists() method"""
@@ -39,12 +39,12 @@ class ProjectTest(unittest.TestCase):
 
     def test_get_yaml(self):
         """Test get_yaml() method"""
-        project_dict = {'name': 'jrgoodle/kit',
+        project_yaml = {'name': 'jrgoodle/kit',
                         'path': 'black-cats/kit',
                         'ref': 'da5c3d32ec2c00aba4a9f7d822cce2c727f7f5dd',
                         'remote': 'origin',
                         'source': 'github'}
-        self.assertEqual(self.project.get_yaml(), project_dict)
+        self.assertEqual(self.project.get_yaml(), project_yaml)
 
 if __name__ == '__main__':
     unittest.main()
