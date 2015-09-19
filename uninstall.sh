3d25b9bdbc917ef191f2aec915ab5cac99b6efe2
joe DeCapo <joe@polka.cat>
2015-09-19 04:29:13 -0500

    Change order of commands in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-sudo -H pip3 uninstall clowder
 sudo -H rm -rf clowder.egg-info
 sudo -H rm -rf dist
 sudo -H rm -rf build
+sudo -H pip3 uninstall clowder
