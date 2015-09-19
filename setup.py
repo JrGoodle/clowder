b8d53bae2ea9c870422a2121dfde58e4b7f24f0b
joe DeCapo <joe@polka.cat>
2015-09-19 04:28:56 -0500

    Add python 3.5 to setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ],
     packages=['clowder', 'clowder.utility'],
     entry_points={
