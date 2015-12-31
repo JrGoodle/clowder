57fbdf74611cf6f40c0888f256e4c90a127a91b3
joe DeCapo <joe@polka.cat>
2015-12-30 19:13:29 -0600

    Fix message in deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-echo "deploy-ios.sh"
+echo "deploy.sh"
 
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "This is a pull request. No deployment will be done."
