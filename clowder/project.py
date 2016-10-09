fbc0e589a61d9468545f37f7a357c44f375c6179
Joe DeCapo <joe@polka.cat>
2016-10-09 13:43:27 -0500

    Use alternate method for obtaining terminal size

--- clowder/project.py
+++ clowder/project.py
@@ -198,6 +198,8 @@ class Project(object):
 
 # Disable warning for unused variables
 # pylint: disable=W0612
+# Disable errors shown by pylint for no specified exception types
+# pylint: disable=W0702
     def _print_status(self):
         """Print formatted project status"""
         repo_path = os.path.join(self.root_directory, self.path)
@@ -207,10 +209,14 @@ class Project(object):
         project_output = format_project_string(repo_path, self.name)
         current_ref_output = format_ref_string(repo_path)
         path_output = colored(self.path, 'cyan')
-        unused_rows, columns = subprocess.check_output(['stty', 'size']).decode().split()
         long_output = project_output + ' ' + current_ref_output + ' -> ' + path_output
+        short_output = project_output + ' ' + current_ref_output + '\n-> ' + path_output
         long_output_length = len(''.join(s for s in long_output if ord(s) > 31 and ord(s) < 126))
-        if long_output_length <= int(columns):
-            print(long_output)
-        else:
-            print(project_output + ' ' + current_ref_output + '\n-> ' + path_output)
+        try:
+            ts = os.get_terminal_size()
+            if long_output_length <= ts.columns:
+                print(long_output)
+            else:
+                print(short_output)
+        except:
+            print(short_output)
