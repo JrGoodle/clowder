17ba1a6e31c688b4e33ccaf39384b963fbbc46d7
Joe DeCapo <joe@polka.cat>
2017-09-18 01:00:31 -0500

    Refactor clowder save test scripts

--- script/tests/test_cats_save.sh
+++ script/tests/test_cats_save.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-TEST_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+test_save()
+{
+    print_separator
+    echo "TEST: Fail linking a previously saved version that doesn't exist"
+    clowder link -v v100 && exit 1
+    echo "TEST: Fail saving a previously saved version"
+    clowder save v0.1 && exit 1
+    echo "TEST: Successfully save a new version"
+    clowder save v0.11 || exit 1
+    echo "TEST: Successfully save version with path separator in input name"
+    clowder save path/separator
+    clowder link -v path-separator || exit 1
+    clowder herd || exit 1
+    clowder status || exit 1
+}
+test_save
+
+test_save_missing_directories()
+{
+    print_separator
+    echo "TEST: Remove directories"
+    rm -rf "$@"
+    echo "TEST: Fail saving version with missing directories"
+    clowder save missing-directories && exit 1
+}
+test_save_missing_directories 'duke' 'mu'
--- script/tests/test_example_cats.sh
+++ script/tests/test_example_cats.sh
@@ -83,35 +83,8 @@ test_branches
 "$TEST_SCRIPT_DIR/tests/test_cats_clean.sh"
 "$TEST_SCRIPT_DIR/tests/test_cats_herd.sh"
 "$TEST_SCRIPT_DIR/tests/test_cats_forall.sh"
-
-test_save()
-{
-    print_separator
-    echo "TEST: Fail linking a previously saved version that doesn't exist"
-    clowder link -v v100 && exit 1
-    echo "TEST: Fail saving a previously saved version"
-    clowder save v0.1 && exit 1
-    echo "TEST: Successfully save a new version"
-    clowder save v0.11 || exit 1
-    echo "TEST: Successfully save version with path separator in input name"
-    clowder save path/separator
-    clowder link -v path-separator || exit 1
-    clowder herd || exit 1
-    clowder status || exit 1
-}
-test_save
-
+"$TEST_SCRIPT_DIR/tests/test_cats_save.sh"
 "$TEST_SCRIPT_DIR/tests/test_cats_stash.sh"
-
-test_save_missing_directories()
-{
-    print_separator
-    echo "TEST: Remove directories"
-    rm -rf "$@"
-    echo "TEST: Fail saving version with missing directories"
-    clowder save missing-directories && exit 1
-}
-test_save_missing_directories 'duke' 'mu'
 
 test_no_versions()
 {
