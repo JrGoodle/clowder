61d4484af37fea0e5957c121cbbabc8d49b3bebc
Joe DeCapo <joe@polka.cat>
2017-09-18 00:54:32 -0500

    Refactor clowder forall test scripts

--- script/tests/test_cats_forall.sh
+++ script/tests/test_cats_forall.sh
@@ -1,3 +1,33 @@
 #!/usr/bin/env bash
 
-TEST_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+test_forall()
+{
+    print_separator
+    echo "TEST: Run forall command"
+    clowder forall -c 'git status' || exit 1
+    echo "TEST: Run forall script"
+    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script.sh" || exit 1
+    echo "TEST: Run forall command for specific groups"
+    clowder forall -c 'git status' -g "$@" || exit 1
+    echo "TEST: Run forall script for specific groups"
+    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script.sh" -g "$@" || exit 1
+    echo "TEST: Run forall command with error"
+    clowder forall -c 'exit 1' && exit 1
+    echo "TEST: Run forall command with --ignore-error"
+    clowder forall -ic 'exit 1' || exit 1
+    echo "TEST: Run forall script with error"
+    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script_error.sh" && exit 1
+    echo "TEST: Run forall script with --ignore-error"
+    clowder forall -ic "$TEST_SCRIPT_DIR/tests/test_forall_script_error.sh" || exit 1
+}
+test_forall 'cats'
+
+test_forall_projects()
+{
+    print_separator
+    echo "TEST: Run forall command for specific projects"
+    clowder forall -c 'git status' -p "$@" || exit 1
+    echo "TEST: Run forall script for specific projects"
+    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script.sh" -p "$@" || exit 1
+}
+test_forall_projects 'jrgoodle/kit' 'jrgoodle/kishka'
--- script/tests/test_example_cats.sh
+++ script/tests/test_example_cats.sh
@@ -82,38 +82,7 @@ test_branches
 "$TEST_SCRIPT_DIR/tests/test_cats_status.sh"
 "$TEST_SCRIPT_DIR/tests/test_cats_clean.sh"
 "$TEST_SCRIPT_DIR/tests/test_cats_herd.sh"
-
-test_forall()
-{
-    print_separator
-    echo "TEST: Run forall command"
-    clowder forall -c 'git status' || exit 1
-    echo "TEST: Run forall script"
-    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script.sh" || exit 1
-    echo "TEST: Run forall command for specific groups"
-    clowder forall -c 'git status' -g "$@" || exit 1
-    echo "TEST: Run forall script for specific groups"
-    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script.sh" -g "$@" || exit 1
-    echo "TEST: Run forall command with error"
-    clowder forall -c 'exit 1' && exit 1
-    echo "TEST: Run forall command with --ignore-error"
-    clowder forall -ic 'exit 1' || exit 1
-    echo "TEST: Run forall script with error"
-    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script_error.sh" && exit 1
-    echo "TEST: Run forall script with --ignore-error"
-    clowder forall -ic "$TEST_SCRIPT_DIR/tests/test_forall_script_error.sh" || exit 1
-}
-test_forall 'cats'
-
-test_forall_projects()
-{
-    print_separator
-    echo "TEST: Run forall command for specific projects"
-    clowder forall -c 'git status' -p "$@" || exit 1
-    echo "TEST: Run forall script for specific projects"
-    clowder forall -c "$TEST_SCRIPT_DIR/tests/test_forall_script.sh" -p "$@" || exit 1
-}
-test_forall_projects 'jrgoodle/kit' 'jrgoodle/kishka'
+"$TEST_SCRIPT_DIR/tests/test_cats_forall.sh"
 
 test_save()
 {
