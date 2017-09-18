bf04a2c878dddc359b7ceed3272d5de7ce3f7825
Joe DeCapo <joe@polka.cat>
2017-09-18 01:08:41 -0500

    Refactor clowder start test scripts

--- script/tests/test_cats_start.sh
+++ script/tests/test_cats_start.sh
@@ -1,3 +1,178 @@
 #!/usr/bin/env bash
 
-TEST_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+test_start()
+{
+    print_separator
+    echo "TEST: Start new branch"
+    clowder herd
+
+    clowder start start_branch || exit 1
+    # TODO: clowder herd -b
+    # clowder herd -b master -g black-cats
+    clowder forall -g black-cats -c 'git fetch origin master'
+    clowder forall -g black-cats -c 'git checkout master'
+
+    pushd mu
+    test_branch start_branch
+    popd
+    pushd duke
+    test_branch start_branch
+    popd
+    pushd black-cats/jules
+    test_branch master
+    popd
+    pushd black-cats/kishka
+    test_branch master
+    popd
+
+    clowder start start_branch || exit 1
+
+    pushd black-cats/jules
+    test_branch start_branch
+    popd
+    pushd black-cats/kishka
+    test_branch start_branch
+    popd
+}
+test_start
+
+if [ -z "$TRAVIS_OS_NAME" ]; then
+    test_start_tracking()
+    {
+        print_separator
+        echo "TEST: Test start tracking branch"
+        clowder herd
+
+        echo "TEST: No local or remote branches"
+        clowder prune -af tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+
+        pushd duke
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd mu
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/jules
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/kishka
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+
+        echo "TEST: Existing local branch checked out, remote tracking branch exists"
+        clowder prune -af tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+
+        echo "TEST: Existing local branch not checked out, remote tracking branch exists"
+        clowder prune -af tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+        clowder forall -c 'git checkout master' || exit 1
+        clowder start -t tracking_branch || exit 1
+
+        echo "TEST: No local branch, existing remote branch"
+        clowder prune -af tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+        clowder prune -f tracking_branch || exit 1
+        clowder start -t tracking_branch && exit 1
+
+        pushd duke
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_no_tracking_branch_exists tracking_branch
+        popd
+        pushd mu
+        test_branch knead
+        test_remote_branch_exists tracking_branch
+        test_no_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/jules
+        test_branch master
+        test_remote_branch_exists tracking_branch
+        test_no_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/kishka
+        test_branch master
+        test_remote_branch_exists tracking_branch
+        test_no_tracking_branch_exists tracking_branch
+        popd
+
+        echo "TEST: Existing local branch checked out, existing remote branch, no tracking relationship"
+        clowder prune -af tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+        clowder prune -f tracking_branch || exit 1
+        clowder forall -c 'git checkout -b tracking_branch' || exit 1
+        clowder start -t tracking_branch && exit 1
+
+        echo "TEST: Existing local branch not checked out, existing remote branch, no tracking relationship"
+        clowder prune -af tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+        clowder prune -f tracking_branch || exit 1
+        clowder forall -c 'git checkout -b tracking_branch' || exit 1
+        clowder forall -c 'git checkout master' || exit 1
+        clowder start -t tracking_branch && exit 1
+
+        echo "TEST: Existing local branch checked out, no remote branch"
+        clowder prune -af tracking_branch
+        clowder start tracking_branch || exit 1
+        clowder start -t tracking_branch || exit 1
+
+        pushd duke
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd mu
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/jules
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/kishka
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+
+        echo "TEST: Existing local branch not checked out, no remote branch"
+        clowder prune -r tracking_branch
+        clowder start tracking_branch || exit 1
+        clowder forall -c 'git checkout master'
+        clowder start -t tracking_branch || exit 1
+
+        pushd duke
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd mu
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/jules
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+        pushd black-cats/kishka
+        test_branch tracking_branch
+        test_remote_branch_exists tracking_branch
+        test_tracking_branch_exists tracking_branch
+        popd
+    }
+    test_start_tracking
+fi
