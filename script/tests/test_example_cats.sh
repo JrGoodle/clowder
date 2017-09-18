40a4c8f719614a395e605c41beb70c5faa80112a
Joe DeCapo <joe@polka.cat>
2017-09-18 00:46:02 -0500

    Refactor clowder clean test scripts

--- script/tests/test_example_cats.sh
+++ script/tests/test_example_cats.sh
@@ -80,46 +80,7 @@ test_branches()
 test_branches
 
 "$TEST_SCRIPT_DIR/tests/test_cats_status.sh"
-
-test_clean()
-{
-    print_separator
-    echo "TEST: Clean repos"
-    make_dirty_repos "${projects[@]}"
-    echo "TEST: Clean specific group when dirty"
-    clowder clean -g "$@" || exit 1
-    clowder status || exit 1
-    echo "TEST: Clean all when dirty"
-    clowder clean || exit 1
-    clowder status || exit 1
-    echo "TEST: Clean when clean"
-    clowder clean || exit 1
-}
-test_clean 'black-cats'
-
-test_clean_projects()
-{
-    print_separator
-    echo "TEST: Clean projects"
-    make_dirty_repos "${projects[@]}"
-    echo "TEST: Clean specific project when dirty"
-    clowder clean -p "$@" || exit 1
-    clowder status || exit 1
-    echo "TEST: Clean all when dirty"
-    clowder clean || exit 1
-    clowder status || exit 1
-}
-test_clean_projects 'jrgoodle/kit'
-
-test_clean_missing_directories()
-{
-    rm -rf "$@"
-    echo "TEST: Discard all changes when directories are missing"
-    clowder clean || exit 1
-    clowder status || exit 1
-    clowder herd || exit 1
-}
-test_clean_missing_directories 'mu' 'duke'
+"$TEST_SCRIPT_DIR/tests/test_cats_clean.sh"
 
 test_herd_dirty_repos()
 {
