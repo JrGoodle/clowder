4f51229e7145dc453d3c5343a7b577aa021f401f
Joe DeCapo <joe@polka.cat>
2017-09-18 01:10:46 -0500

    Refactor clowder import test scripts

--- script/tests/test_example_cats.sh
+++ script/tests/test_example_cats.sh
@@ -111,47 +111,7 @@ test_clowder_repo()
 }
 test_clowder_repo
 
-test_clowder_import()
-{
-    print_separator
-    echo "TEST: Test clowder file with default import"
-
-    clowder link
-    clowder herd
-    clowder link -v import-default
-    clowder herd
-    pushd black-cats/jules
-    test_branch import-default
-    popd
-    pushd black-cats/kishka
-    test_branch import-default
-    popd
-    pushd black-cats/kit
-    test_branch import-default
-    popd
-    pushd black-cats/sasha
-    test_branch import-default
-    popd
-
-    echo "TEST: Test clowder file with version import"
-    clowder link
-    clowder herd
-    clowder link -v import-version
-    clowder herd
-    pushd black-cats/jules
-    test_branch import-version
-    popd
-    pushd black-cats/kishka
-    test_branch import-version
-    popd
-    pushd black-cats/kit
-    test_branch import-version
-    popd
-    pushd black-cats/sasha
-    test_branch import-version
-    popd
-}
-test_clowder_import
+"$TEST_SCRIPT_DIR/tests/test_cats_import.sh"
 
 test_print()
 {
