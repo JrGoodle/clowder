99582e865757426af9194ec20e77c07f68dcfd08
Joe DeCapo <joe@polka.cat>
2017-09-28 20:31:19 -0500

    Remove popen execute functions

--- clowder/utility/clowder_utilities.py
+++ clowder/utility/clowder_utilities.py
@@ -45,25 +45,6 @@ def execute_forall_command(cmd, path, clowder_path, name, remote, fork_remote, r
                            cwd=path,
                            env=forall_env)
 
-def execute_command_popen(cmd, path):
-    """Execute command and display continuous output"""
-    for output in execute_popen(cmd, path):
-        print(output, end='')
-
-def execute_popen(cmd, path):
-    """Execute command"""
-    # https://stackoverflow.com/questions/4417546/constantly-print-subprocess-output-while-process-is-running
-    process = subprocess.Popen(cmd, cwd=path,
-                               stdout=subprocess.PIPE,
-                               universal_newlines=True)
-    # print("the commandline is {}".format(process.args))
-    for stdout_line in iter(process.stdout.readline, ''):
-        yield stdout_line
-    process.stdout.close()
-    return_code = process.wait()
-    if return_code:
-        raise subprocess.CalledProcessError(return_code, cmd)
-
 def force_symlink(file1, file2):
     """Force symlink creation"""
     try:
