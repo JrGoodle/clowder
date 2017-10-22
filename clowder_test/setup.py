7cd494b2ab52e88d5329dd273e6cfdf0d7fb538f
Joe DeCapo <joe@polka.cat>
2017-10-21 20:18:25 -0500

    Add termcolore to clowder_test setup.py

--- clowder_test/setup.py
+++ clowder_test/setup.py
@@ -33,5 +33,5 @@ setup(
             'clowder-test=clowder_test.cmd:main',
         ]
     },
-    install_requires=['argcomplete', 'cprint', 'psutil']
+    install_requires=['argcomplete', 'cprint', 'psutil', 'termcolor']
 )
