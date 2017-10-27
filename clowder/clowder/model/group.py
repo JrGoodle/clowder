ece1d74a5002d2013fb1d8156a9f7ac70b6ad8c7
John Lane <jlane@sportinginnovations.com>
2017-10-27 15:20:00 -0500

    Update Sphinx documentation for model.group

--- clowder/clowder/model/group.py
+++ clowder/clowder/model/group.py
@@ -1,4 +1,9 @@
-"""Representation of clowder.yaml group"""
+# -*- coding: utf-8 -*-
+"""Representation of clowder.yaml group
+
+.. codeauthor:: Joe Decapo <joe@polka.cat>
+
+"""
 
 from __future__ import print_function
 
@@ -28,23 +33,41 @@ class Group(object):
         self.projects.sort(key=lambda p: p.path)
 
     def existing_branch(self, branch, is_remote):
-        """Checks whether at least one branch exists"""
+        """Checks if given branch exists in any project
+
+        :param str branch: Branch to check for
+        :param bool is_remote:
+        :return: True, if at least one branch exists
+        :rtype: bool
+        """
 
         return any([p.existing_branch(branch, is_remote=is_remote) for p in self.projects])
 
     def existing_projects(self):
-        """Validate existence status of all projects"""
+        """Validate existence status of all projects
+
+        :return: True, if all projects exist
+        :rtype: bool
+        """
 
         return all([ProjectRepo.existing_git_repository(project.full_path()) for project in self.projects])
 
     def get_yaml(self):
-        """Return python object representation for saving yaml"""
+        """Return python object representation for saving yaml
+
+        :return: YAML python object
+        :rtype: dict
+        """
 
         projects_yaml = [p.get_yaml() for p in self.projects]
         return {'name': self.name, 'projects': projects_yaml}
 
     def get_yaml_resolved(self):
-        """Return python object representation for resolved yaml"""
+        """Return python object representation for resolved yaml
+
+        :return: YAML python object
+        :rtype: dict
+        """
 
         projects_yaml = [p.get_yaml(resolved=True) for p in self.projects]
 
@@ -62,17 +85,28 @@ class Group(object):
         return group
 
     def is_dirty(self):
-        """Check if group has at least one dirty project"""
+        """Check if group has at least one dirty project
+
+        :return: True, if any project is dirty
+        :rtype: bool
+        """
 
         return any([project.is_dirty() for project in self.projects])
 
     def is_valid(self):
-        """Validate status of all projects"""
+        """Validate status of all projects
+
+        :return: True, if all projects are valid
+        :rtype: bool
+        """
 
         return all([project.is_valid() for project in self.projects])
 
     def print_existence_message(self):
-        """Print existence validation message for projects in group"""
+        """Print existence validation message for projects in group
+
+        :return:
+        """
 
         if self.existing_projects():
             return
@@ -84,7 +118,10 @@ class Group(object):
                 ProjectRepo.existing_git_repository(project.full_path())
 
     def print_validation(self):
-        """Print validation message for projects in group"""
+        """Print validation message for projects in group
+
+        :return:
+        """
 
         if self.is_valid():
             return
