f8895148a1f8fef09268fbd3314c2cf71912859c
John Lane <jlane@sportinginnovations.com>
2017-10-29 13:55:38 -0500

    Add Sphinx documentation to validate_clowder_repo method

--- clowder/clowder/util/decorators.py
+++ clowder/clowder/util/decorators.py
@@ -112,7 +112,11 @@ def valid_clowder_yaml_required(func):
 
 
 def _validate_clowder_repo_exists(repo):
-    """If clowder repo doesn't exist, print message and exit"""
+    """If clowder repo doesn't exist, print message and exit
+
+    :param ClowderRepo repo: Repo to check
+    :return:
+    """
 
     if repo is None:
         cprint(' - No clowder found in the current directory\n', 'red')
