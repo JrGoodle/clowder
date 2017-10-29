48a1cce4f2d6722baeda3e17406b012feb2c097e
Joe DeCapo <joe@polka.cat>
2017-10-29 15:20:10 -0500

    Refactor clowder_controller methods to use **kwargs

--- clowder/clowder/util/decorators.py
+++ clowder/clowder/util/decorators.py
@@ -26,19 +26,6 @@ def clowder_required(func):
     return wrapper
 
 
-def initialize_skip_projects(func):
-    """Initialize skip projects to empty list if necessary"""
-
-    def wrapper(*args, **kwargs):
-        """Wrapper"""
-
-        if kwargs['skip'] is None:
-            kwargs['skip'] = []
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
 def not_detached(func):
     """If HEAD is detached, print error message and exit"""
 
