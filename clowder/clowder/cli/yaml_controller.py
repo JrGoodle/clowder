d9f388d2b486da3bd5e3209db70d3e691aec584d
Joe DeCapo <joe@polka.cat>
2017-11-02 22:30:36 -0500

    Add `clowder yaml` logic to Cement controller

--- clowder/clowder/cli/yaml_controller.py
+++ clowder/clowder/cli/yaml_controller.py
@@ -1,6 +1,16 @@
+from __future__ import print_function
+
+import sys
+
 from cement.ext.ext_argparse import expose
 
+import clowder.util.formatting as fmt
 from clowder.cli.abstract_base_controller import AbstractBaseController
+from clowder.util.decorators import (
+    print_clowder_repo_status,
+    valid_clowder_yaml_required
+)
+from clowder.yaml.printing import print_yaml
 
 
 class YAMLController(AbstractBaseController):
@@ -14,5 +24,11 @@ class YAMLController(AbstractBaseController):
             ]
 
     @expose(help="second-controller default command", hide=True)
+    @valid_clowder_yaml_required
+    @print_clowder_repo_status
     def default(self):
-        print("Inside SecondController.default()")
+        if self.app.pargs.resolved:
+            print(fmt.yaml_string(self.clowder.get_yaml_resolved()))
+        else:
+            print_yaml(self.clowder.root_directory)
+        sys.exit()  # exit early to prevent printing extra newline
