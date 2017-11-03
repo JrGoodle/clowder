0728a5b64ec9a871267d3b0b6ea6c3390b7a8e1f
Joe DeCapo <joe@polka.cat>
2017-11-02 21:36:20 -0500

    Add `clowder status` logic to Cement controller

--- clowder/clowder/cli/status_controller.py
+++ clowder/clowder/cli/status_controller.py
@@ -1,6 +1,9 @@
 from cement.ext.ext_argparse import expose
 
+import clowder.util.formatting as fmt
 from clowder.cli.abstract_base_controller import AbstractBaseController
+from clowder.commands.util import run_group_command
+from clowder.util.decorators import network_connection_required
 
 
 class StatusController(AbstractBaseController):
@@ -15,4 +18,29 @@ class StatusController(AbstractBaseController):
 
     @expose(help="second-controller default command", hide=True)
     def default(self):
-        print("Inside SecondController.default()")
+        if self.app.pargs.fetch:
+            _fetch_projects(self.clowder_repo, self.clowder)
+        else:
+            self.clowder_repo.print_status()
+
+        padding = len(max(self.clowder.get_all_project_paths(), key=len))
+
+        for group in self.clowder.groups:
+            print(fmt.group_name(group.name))
+            for project in group.projects:
+                print(project.status(padding=padding))
+
+
+@network_connection_required
+def _fetch_projects(clowder_repo, clowder):
+    """fetch all projects
+
+    :param ClowderRepo clowder_repo: ClowderRepo instance
+    :param ClowderController clowder: ClowderController instance
+    """
+
+    clowder_repo.print_status(fetch=True)
+
+    print(' - Fetch upstream changes for projects\n')
+    for group in clowder.groups:
+        run_group_command(group, [], 'fetch_all')
