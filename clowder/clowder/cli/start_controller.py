47f04b05f642cd7ec8f94c5517cffd8263b81684
Joe DeCapo <joe@polka.cat>
2017-11-02 22:23:46 -0500

    Add `clowder start` logic to Cement controller

--- clowder/clowder/cli/start_controller.py
+++ clowder/clowder/cli/start_controller.py
@@ -1,6 +1,19 @@
 from cement.ext.ext_argparse import expose
 
 from clowder.cli.abstract_base_controller import AbstractBaseController
+from clowder.commands.util import (
+    filter_groups,
+    filter_projects_on_project_names,
+    run_group_command,
+    run_project_command,
+    validate_groups,
+    validate_projects
+)
+from clowder.util.decorators import (
+    print_clowder_repo_status,
+    network_connection_required,
+    valid_clowder_yaml_required
+)
 
 
 class StartController(AbstractBaseController):
@@ -15,5 +28,60 @@ class StartController(AbstractBaseController):
             ]
 
     @expose(help="second-controller default command", hide=True)
+    @valid_clowder_yaml_required
+    @print_clowder_repo_status
     def default(self):
-        print("Inside SecondController.default()")
+        if self.app.pargs.tracking:
+            self._start_tracking()
+            return
+
+        if self.app.pargs.projects is None:
+            _start_groups(self.clowder, self.app.pargs.groups, self.app.pargs.skip, self.app.pargs.branch)
+        else:
+            _start_projects(self.clowder, self.app.pargs.projects, self.app.pargs.skip, self.app.pargs.branch)
+
+    @network_connection_required
+    def _start_tracking(self):
+        """clowder start tracking command"""
+
+        if self.app.pargs.projects is None:
+            _start_groups(self.clowder, self.app.pargs.groups, self.app.pargs.skip,
+                          self.app.pargs.branch, tracking=True)
+            return
+
+        _start_projects(self.clowder, self.app.pargs.projects, self.app.pargs.skip,
+                        self.app.pargs.branch, tracking=True)
+
+
+def _start_groups(clowder, group_names, skip, branch, tracking=False):
+    """Start feature branch for groups
+
+    :param ClowderController clowder: ClowderController instance
+    :param list[str] group_names: Group names to create branches for
+    :param list[str] skip: Project names to skip
+    :param str branch: Local branch name to create
+    :param Optional[bool] tracking: Whether to create a remote branch with tracking relationship.
+        Defaults to False
+    """
+
+    groups = filter_groups(clowder.groups, group_names)
+    validate_groups(groups)
+    for group in groups:
+        run_group_command(group, skip, 'start', branch, tracking)
+
+
+def _start_projects(clowder, project_names, skip, branch, tracking=False):
+    """Start feature branch for projects
+
+    :param ClowderController clowder: ClowderController instance
+    :param list[str] project_names: Project names to creat branches for
+    :param list[str] skip: Project names to skip
+    :param str branch: Local branch name to create
+    :param Optional[bool] tracking: Whether to create a remote branch with tracking relationship.
+        Defaults to False
+    """
+
+    projects = filter_projects_on_project_names(clowder.groups, project_names)
+    validate_projects(projects)
+    for project in projects:
+        run_project_command(project, skip, 'start', branch, tracking)
