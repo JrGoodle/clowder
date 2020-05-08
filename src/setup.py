48ece14dbd01e4750926b677919a08496eeb8906
Joe DeCapo <joe@polka.cat>
2020-05-07 19:00:43 -0500

    Add Python 3.8 support

--- src/setup.py
+++ src/setup.py
@@ -21,7 +21,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     packages=['clowder',
               'clowder.cli',
