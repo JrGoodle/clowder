ff03e1c1eb3a11c45f020e04f07e19031a4e9538
Joe DeCapo <joe@polka.cat>
2020-08-22 21:24:15 -0500

    More test

--- src/tests/features/new_syntax.feature
+++ src/tests/features/new_syntax.feature
@@ -1,17 +1,23 @@
 Feature: New Syntax
     Run 'clowder init', 'clowder herd', and 'clowder status'
 
-    Scenario: Default init
-        Given I'm using the default cats clowder.yml
-        And I'm in an empty directory
+    Scenario Outline: Default init
+        Given cats example is initialized
+        And <directory> doesn't exist
 
-        When I run 'clowder init git@github.com:JrGoodle/cats.git'
-        And I run 'clowder herd'
+        When I run 'clowder herd'
         And I run 'clowder status'
 
-        Then Project at directory mu is on branch knead
-        And Project at directory duke is on branch heads/purr
-        And Project at directory black-cats/kishka is on branch master
-        And Project at directory black-cats/kit is on branch master
-        And Project at directory black-cats/sasha is on branch master
-        And Project at directory black-cats/june is on branch master
+        Then project at <directory> exists
+        And project at <directory> is a git repository
+        And project at <directory> is on <branch>
+        And project at <directory> is clean
+
+        Examples:
+        | directory         | branch |
+        | mu                | knead  |
+        | duke              | purr   |
+        | black-cats/kishka | master |
+        | black-cats/kit    | master |
+        | black-cats/sasha  | master |
+        | black-cats/june   | master |
--- src/tests/functional/test_new_syntax.py
+++ src/tests/functional/test_new_syntax.py
@@ -2,16 +2,47 @@
 
 import os
 import subprocess
+from pathlib import Path
 
+from git import Repo
 # noinspection PyPackageRequirements
 from pytest_bdd import scenarios, given, when, then, parsers
 
 scenarios('../features/new_syntax.feature')
 
+TEST_REPOS = {
+    "cats": {"url": "github.com", "name": "JrGoodle/cats"},
+    "misc": {"url": "github.com", "name": "JrGoodle/misc-clowder-tests"},
+    "swift": {"url": "github.com", "name": "JrGoodle/swift-clowder"}
+}
 
-@given("I'm using the default cats clowder.yml")
-def step_impl():
-    pass
+
+@given(parsers.parse("{example} example is initialized"))
+def example_init(tmpdir, example):
+    url = get_url(example)
+    command = f"clowder init {url}"
+    run_command(command, tmpdir)
+
+
+@given(parsers.parse("{example} example is initialized to {branch}"))
+def example_init_branch(tmpdir, example, branch):
+    url = get_url(example)
+    command = f"clowder init {url} -b {branch}"
+    run_command(command, tmpdir)
+
+
+@given(parsers.parse("{example} example is initialized with {protocol}"))
+def example_init_branch_protocol(tmpdir, example, protocol):
+    url = get_url(example, protocol=protocol)
+    command = f"clowder init {url}"
+    run_command(command, tmpdir)
+
+
+@given(parsers.parse("{example} example is initialized to {branch} with {protocol}"))
+def example_init_branch_protocol(tmpdir, example, branch, protocol):
+    url = get_url(example, protocol=protocol)
+    command = f"clowder init {url} -b {branch}"
+    run_command(command, tmpdir)
 
 
 @given("I'm in an empty directory")
@@ -20,16 +51,58 @@ def is_empty_directory(tmpdir):
     assert is_directory_empty(tmpdir)
 
 
+@given("<directory> doesn't exist")
+def has_no_directory(tmpdir, directory):
+    path = Path(tmpdir / directory)
+    assert not path.exists()
+
+
+@then("project at <directory> exists")
+def has_directory(tmpdir, directory):
+    path = Path(tmpdir / directory)
+    assert path.exists()
+
+
+@then("project at <directory> is a git repository")
+def is_git_repo(tmpdir, directory):
+    path = Path(tmpdir / directory)
+    assert path.exists()
+    assert has_git_directory(path)
+
+
 @when(parsers.parse("I run 'clowder {command}'"))
 def run_clowder(tmpdir, command):
-    # pipe = None if print_output else subprocess.PIPE
-    subprocess.run(f"clowder {command}", shell=True, cwd=tmpdir, check=True)
+    run_command(f"clowder {command}", tmpdir)
 
 
-@then(parsers.parse("Project at directory {directory} is on branch {branch}"))
-def check_directory_branch(directory, branch):
-    # Check branch at directory path
-    pass
+@then("project at <directory> is on <branch>")
+def check_directory_branch(tmpdir, directory, branch):
+    path = Path(tmpdir / directory)
+    repo = Repo(str(path))
+    active_branch = repo.active_branch.name
+    print(f"TEST: active brach '{active_branch}' is '{branch}'")
+    assert active_branch == branch
+
+
+@then("project at <directory> is clean")
+def check_directory_clean(tmpdir, directory):
+    path = Path(tmpdir / directory)
+    repo = Repo(str(path))
+    print(f"TEST: Project at {directory} is clean")
+    assert not is_dirty(repo)
+
+
+def is_dirty(repo) -> bool:
+    """Check whether repo is dirty
+
+    :return: True, if repo is dirty
+    :rtype: bool
+    """
+
+    # if not self.repo_path.is_dir():
+    #     return False
+
+    return repo.is_dirty()  # or self._is_rebase_in_progress() or self._has_untracked_files()
 
 
 def is_directory_empty(dir_name):
@@ -43,3 +116,27 @@ def is_directory_empty(dir_name):
     else:
         print("Given Directory don't exists")
         raise Exception
+
+
+def has_git_directory(dir_name):
+    path = Path(dir_name / ".git")
+    return path.is_dir()
+
+
+def run_command(command, path):
+    print(f"TEST: {command}")
+    # pipe = None if print_output else subprocess.PIPE
+    subprocess.run(command, shell=True, cwd=path, check=True)
+
+
+def get_url(example, protocol="ssh"):
+    source_url = TEST_REPOS[example]["url"]
+    name = TEST_REPOS[example]["name"]
+    if protocol == "ssh":
+        url = f"git@{source_url}:{name}.git"
+    elif protocol == "https":
+        url = f"https://{source_url}/{name}.git"
+    else:
+        raise Exception
+    print(f"TEST: {url}")
+    return url
