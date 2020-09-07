b21b01e41ea2b31ba269a2ad74a2b38a0951cc4e
Joe DeCapo <joe@polka.cat>
2020-09-07 13:47:15 -0500

    Try uninstalling more lfs

--- tests/functional/given/git.py
+++ tests/functional/given/git.py
@@ -131,9 +131,10 @@ def given_test_dir_is_git_repo(tmp_path: Path, test_directory: str) -> None:
 
 @given("project at <directory> doesn't have lfs installed")
 def given_has_no_lfs_installed(tmp_path: Path, directory: str) -> None:
-    result = util.run_command("git lfs uninstall --skip-repo", tmp_path)
-    assert result.returncode == 0
     path = tmp_path / directory
+    util.run_command("git lfs uninstall --local", path)
+    util.run_command("git lfs uninstall --system", path)
+    util.run_command("git lfs uninstall", path)
     assert not util.lfs_hooks_installed(path)
     assert not util.lfs_filters_installed(path)
 
