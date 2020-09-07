1ae0664807d4250606bfd9e5876af5d9816fb147
Joe DeCapo <joe@polka.cat>
2020-09-07 16:24:39 -0500

    Add debug output

--- tests/functional/then/command_status.py
+++ tests/functional/then/command_status.py
@@ -4,13 +4,16 @@ from pathlib import Path
 
 from pytest_bdd import then, parsers
 
+import tests.functional.util as util
 from tests.functional.util import CommandResults, ScenarioInfo
 
 
 @then("the command succeeds")
 def then_command_succeeded(command_results: CommandResults, scenario_info: ScenarioInfo, tmp_path: Path) -> None:
+    # DEBUG
+    util.list_git_config(tmp_path / "mu")
     assert len(command_results.completed_processes) == 1
-    assert all([result.returncode == 0 for result in command_results.completed_processes])
+    assert all([r.returncode == 0 for r in command_results.completed_processes])
 
 
 @then("the commands succeed")
