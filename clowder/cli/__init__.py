3f0e13f72213f2c833a33ee4fbaf308d6af64880
Joe DeCapo <joe@polka.cat>
2020-12-06 22:11:46 -0600

    wip

--- clowder/cli/__init__.py
+++ clowder/cli/__init__.py
@@ -4,21 +4,21 @@
 
 """
 
-import clowder.cli.branch
-import clowder.cli.checkout
-import clowder.cli.clean
-import clowder.cli.config
-import clowder.cli.diff
-import clowder.cli.forall
-import clowder.cli.herd
-import clowder.cli.init
-import clowder.cli.link
-# import clowder.cli.plugins
-import clowder.cli.prune
-import clowder.cli.repo
-import clowder.cli.reset
-import clowder.cli.save
-import clowder.cli.start
-import clowder.cli.stash
-import clowder.cli.status
-import clowder.cli.yaml
+from clowder.cli.branch import BranchCommand
+from clowder.cli.checkout import CheckoutCommand
+from clowder.cli.clean import CleanCommand
+from clowder.cli.config import ConfigCommand
+from clowder.cli.diff import DiffCommand
+from clowder.cli.forall import ForallCommand
+from clowder.cli.herd import HerdCommand
+from clowder.cli.init import InitCommand
+from clowder.cli.link import LinkCommand
+# from clowder.cli.plugins import PluginCommands
+from clowder.cli.prune import PruneCommand
+from clowder.cli.repo import RepoCommand
+from clowder.cli.reset import ResetCommand
+from clowder.cli.save import SaveCommand
+from clowder.cli.start import StartCommand
+from clowder.cli.stash import StashCommand
+from clowder.cli.status import StatusCommand
+from clowder.cli.yaml import YamlCommand
