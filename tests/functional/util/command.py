7ce06a328e65b177c3e83a7c7abca860ef49f66f
Joe DeCapo <joe@polka.cat>
2023-03-23 23:18:15 -0500

    print PATH

--- tests/functional/util/command.py
+++ tests/functional/util/command.py
@@ -17,6 +17,10 @@ class CommandResults:
 def run_command(command: str, cwd: Path, check: bool = False) -> CompletedProcess:
     env = {"DEBUG": "true"}
     processed_command = _process_clowder_commands(command)
+    CONSOLE.stdout('echo $PATH')
+    result = cmd.run('echo $PATH', cwd=cwd, check=check, print_output=False, print_command=True, env=env)
+    CONSOLE.stdout(result.stdout.strip())
+    CONSOLE.stdout(result.stderr.strip())
     result = cmd.run(processed_command, cwd=cwd, check=check, print_output=False, print_command=True, env=env)
 
     output = result.stdout.strip()
